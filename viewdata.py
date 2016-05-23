2b84ead1ed05f46c655118493b778d6a3fe7aad0
Glaceon31 <grit31@126.com>
2016-05-23 15:08:10 +0800

    view data

---
+++ viewdata.py
@@ -0,0 +1,45 @@
+#-*- coding: utf-8 -*-
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from werkzeug import secure_filename
+import json
+import os
+
+app = Flask(__name__)
+app.debug = True
+
+import pymongo
+import traceback
+from pymongo import MongoClient
+
+client = MongoClient(connect=False)
+
+db = client.thusummercamp
+userdb = db.user
+
+@app.route('/viewdata')
+def viewdata():
+	return render_template('dataviewer.html')
+
+@app.route('/getviewdata')
+def getviewdata():
+	result = []
+	tmp = userdb.find({'applied':True})
+	for i in tmp:
+		user = {}
+		user['username'] = i['username']
+		user['name'] = i['name']
+		user['college'] = i['college']
+		user['identity'] = i['identity']
+		user['email'] = i['email']
+		user['department'] = i['department']
+		result.append(user)
+	return json.dumps(result)
+
+@app.route('/getmaterial/<username>', methods=['GET','POST'])
+def getmaterial(username):
+	tmp = userdb.find_one({'username': username})
+	directory=os.path.join('applications',username)
+	return send_from_directory(directory=directory,filename=tmp['filename'], as_attachment=True)
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port = 8552)
